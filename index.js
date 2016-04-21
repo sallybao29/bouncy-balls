83229e2b51f6a46476f9828c7554858300fe53b4
lorenrose1013 <lorenrose1013@aol.com>
2016-04-21 12:34:01 -0400

    began to animate

--- index.js
+++ index.js
@@ -7,31 +7,44 @@ var balls = [];
 
 var makeBall = function(){
     var shape = document.createElementNS(svgns, "circle");
-    var x = y = 25;
-    var xVel = yVel = 3;
+    var x = 25;
+    var y = 25;
+    var xVel = 3; 
+    var yVel = 3;
     shape.setAttribute("cx", x);
     shape.setAttribute("cy", y);
     shape.setAttribute("r",  20);
-    shape.setAttribute("fill", "green"); 
+    shape.setAttribute("fill", "red"); 
     pic.appendChild(shape);
     
-    var mov = function(){
+    var movef = function(){
 	x = x + xVel;
 	y = y + yVel;
 	shape.setAttribute("cx", x);
 	shape.setAttribute("cy", y);
     }
-    var setX = function(X){ x = X; shape.setAttribute("cx", x);};
-    var setY = function(Y){ y = Y; shape.setAttribute("cy", y);};
-    var setXVel = function(X){ xVel = X;};
-    var setYVel = function(Y){ yVel = Y;};
-    balls.push(shape);
-    return {
-	move : move,
-	setX : setX,
-	setY : setY,
-	setXVel : setXVel,
-	setYVel : setYVel
+    var setXf = function(X){ x = X; shape.setAttribute("cx", x);};
+    var setYf = function(Y){ y = Y; shape.setAttribute("cy", y);};
+    var setXVelf = function(X){ xVel = X;};
+    var setYVelf = function(Y){ yVel = Y;};
+
+    var obj =  {
+	move : movef,
+	setX : setXf,
+	setY : setYf,
+	setXVel : setXVelf,
+	setYVel : setYVelf
     }
+    balls.push(obj);    
 };
 
+
+var requestId;
+var animate = function(){
+    for( ball in balls){
+	ball.move();
+    }
+    
+    requestId = window.requestAnimationFrame(animate);
+};
+animate();
